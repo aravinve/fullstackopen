dbf21952484190dacad36b307165c6201d3dd5f4
Aravind Venkatesan <aravindtherocker@gmail.com>
2021-01-11 17:34:24 +0800

    part1_ex_1.12_commit

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -1,16 +1,40 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const App = () => {
+const App = (props) => {
+  const [selected, setSelected] = useState(0)
+
+  const handleClick = () => {
+    const randomNumber = Math.floor(Math.random() * anecdotes.length)
+    setSelected(randomNumber)
+  }
+
   return (
-    <h1>Hello</h1>
+    <>
+      <h1>Anectode of the day</h1>
+      <div>
+        {props.anecdotes[selected]}
+      </div>
+      <div>
+        <button onClick={handleClick}>Next</button>
+      </div>
+    </>
   )
 }
 
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App anecdotes={anecdotes} />
   </React.StrictMode>,
   document.getElementById('root')
 );
