31cf0a033d4fa1f45c19eb1c60af8480a22bf23a
Aravind Venkatesan <aravindtherocker@gmail.com>
2021-01-11 16:49:14 +0800

    part1_ex_1.6_commit

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,11 +1,57 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+const Button = ({text, handler}) => {
+  return (
+    <button onClick={handler}>
+      {text}
+    </button>
+  )
+}
+
+const Header = ({headerText, goodHandler, neutralHandler, badHandler}) => {
+  return (
+    <>
+      <h1>{headerText}</h1>
+      <Button text="good" handler={goodHandler} />
+      <Button text="neutral" handler={neutralHandler} />
+      <Button text="bad" handler={badHandler} />
+    </>
+  )
+}
+
+const Stats = ({headerText, good, neutral, bad}) => {
+  return (
+    <>
+    <h1>{headerText}</h1>
+    <p>
+      Good {good}
+    </p>
+    <p>
+      Neutral {neutral}
+    </p>
+    <p>
+      Bad {bad}
+    </p>
+    </>
+  )
+}
 
 const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const goodHandler = () => setGood(good + 1)
+  const neutralHandler = () => setNeutral(neutral + 1)
+  const badHandler = () => setBad(bad + 1)
+
   return (
-    <h1>Hello</h1>
+    <div>
+      <Header headerText="Give Feedback" goodHandler={goodHandler} neutralHandler={neutralHandler} badHandler={badHandler} />
+      <Stats headerText="Statistics" good={good} neutral={neutral} bad={bad} />
+    </div>
   )
 }
 
